16ba1b9e680ccf8ad316194c44ab161382ea0b88
maxlee <maxplee8@gmail.com>
2023-12-16 22:04:59 -0500

    wish i could test

--- pages/music/index.js
+++ pages/music/index.js
@@ -7,6 +7,7 @@ export default function Music() {
     const CLIENT_ID = "5076e52b92314231bf0104f7a0048eba";
     const CLIENT_SECRET = "1b90606d9ea14fdda1acb4cd2ede5a60";
     const [accesstoken, setAccessToken] = useState("");
+    const [tracks, setTracks] = useState([]);
     const redirect_uri = "https://max-lee.dev/music";
     useEffect(() => {
         var authParameters = {
@@ -43,6 +44,7 @@ export default function Music() {
             })
                 .then((res) => res.json())
                 .then((data) => {
+
                     console.log(data)
                     setAccessToken(data.access_token)
                 });
@@ -55,13 +57,26 @@ export default function Music() {
 
     async function myData() {
         // get user my details
-        const response = await fetch('https://api.spotify.com/v1/users/rjji4jtqgoxgpsvpvblnp9ylr ', {
+        const response = await fetch('https://api.spotify.com/v1/me/top/artists', {
             headers: {
-                Authorization: `Bearer ${accesstoken}`
-            }
-        })
-        const data = await response.json()
+                Authorization: `Bearer ${accesstoken}`,
+            },
+        });
+
+        const data = await response.json();
         console.log(data)
+        let tracks = data.items.map((track) => {
+                return {
+                    artist: track.name,
+                    song: track.external_urls.spotify,
+                    image: track.images[0].url,
+                };
+
+            }
+        );
+
+        setTracks(tracks)
+
 
     }
 
@@ -84,6 +99,15 @@ export default function Music() {
                     <h1>music</h1>
                     <button onClick={requestAuthorization}>login</button>
                     <Text>{accesstoken} updated</Text>
+                    <button onClick={myData}>get data</button>
+                    <ul>
+                        {tracks.map((track) => (
+                            <li key={track.song}>
+                                <img src={track.image} alt={track.song}/>
+                                <a href={track.song}>{track.artist}</a>
+                            </li>
+                        ))}
+                    </ul>
                 </Box>
             </main>
         </>
