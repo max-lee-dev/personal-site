6417867d5526f91b8caa9947469b7c87471700b8
maxlee <maxplee8@gmail.com>
2023-12-24 02:31:42 -0500

    apos;

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,8 @@ const inter = Inter({subsets: ['latin']})
 export default function Home() {
     return (
         <Box color={'brandGray.100'} fontFamily={"Lexend Deca"}>
-            <Text color={'brandWhite.100'} fontSize={'40px'}>hey im max </Text>
-            <Text paddingTop={5} fontSize={'24px'}>I'm an aspiring full-stack developer who currently attends high
+            <Text color={'brandWhite.100'} fontSize={'40px'}>hey i&apos;m max </Text>
+            <Text paddingTop={5} fontSize={'24px'}>I&apos;m an aspiring full-stack developer who currently attends high
                 school in New York.
                 I enjoy running mid/long distance while getting better at programming in my free
                 time.</Text>
