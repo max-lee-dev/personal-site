7cc7c37c57236611321a51eebbb0dd2722beb17b
maxlee <maxplee8@gmail.com>
2024-11-03 18:24:41 -0500

    👳‍♀️ add 2 more

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -5,6 +5,8 @@ import ResumeSwap from "../../assets/resumeswap2.png";
 import findecs from "../../assets/findecs.png";
 import gnsscioly from "../../assets/gnsscioly.png";
 import TennisConnect from "../../assets/TennisConnect.png";
+import movie from "../../assets/movie.png";
+import Smash from "../../assets/Smash.png";
 
 export const projectsData = [
   {
@@ -53,6 +55,23 @@ export const projectsData = [
     link1: "https://gnsscioly.com",
     link2: "https://github.com/max-lee-dev/gns-scioly"
   },
+  {
+    id: 7,
+    image: movie,
+    title: "movie-web-scraper",
+    category: "web app",
+    link1: "https://movie-web-scraper.vercel.app",
+    link2: "https://github.com/max-lee-dev/movie-web-scraper"
+  },
+  {
+    id: 8,
+    image: Smash,
+    title: "SmashWhosBetterAt",
+    category: "web app",
+    link1: "https://smashwhosbetterat.com",
+    link2: "https://github.com/max-lee-dev/smashwhosbetterat"
+
+  }
 
 ];
 
